c31bc8401809f0a08adf98a45e1f14c7a9a7e615
woaibocai <365191806@qq.com>
2023-11-14 12:46:51 +0800

    更新refresh_token值

--- woaibocai-manager/src/main/java/top/woaibocai/manager/mapper/UserMapper.java
+++ woaibocai-manager/src/main/java/top/woaibocai/manager/mapper/UserMapper.java
@@ -8,4 +8,6 @@ import top.woaibocai.model.common.User;
 public interface UserMapper extends BaseMapper<User> {
     //根据userName查询数据库
     User selectByUserName(String userName);
+
+    void updateOfRefreshToken(String refreshToken,Long id);
 }
