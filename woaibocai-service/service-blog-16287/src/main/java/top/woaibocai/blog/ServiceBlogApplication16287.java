823f26454f9ee2215e15a6c5b5e9f8e3a44a63cc
bocai <365191806@qq.com>
2023-11-29 13:30:04 +0800

    添加博客模块

---
+++ woaibocai-service/service-blog-16287/src/main/java/top/woaibocai/blog/ServiceBlogApplication16287.java
@@ -0,0 +1,13 @@
+package top.woaibocai.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class ServiceBlogApplication16287 {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceBlogApplication16287.class,args);
+    }
+}
