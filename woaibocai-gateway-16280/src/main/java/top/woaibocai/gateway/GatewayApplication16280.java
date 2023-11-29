4aff229a8e46b1d98ead8c99904cd11e34c52d56
bocai <365191806@qq.com>
2023-11-29 13:29:46 +0800

    添加网关模块

---
+++ woaibocai-gateway-16280/src/main/java/top/woaibocai/gateway/GatewayApplication16280.java
@@ -0,0 +1,11 @@
+package top.woaibocai.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication16280 {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication16280.class,args);
+    }
+}
