29867a03f2a17cbb320f4e70999ce6e3fa32b4da
woaibocai <365191806@qq.com>
2023-12-09 15:17:26 +0800

    创建service-user-16281模块 主要处理用户交互的所有接口

---
+++ woaibocai-service/service-user-16281/src/main/java/top/woaibocai/user/ServiceUserApplication16281.java
@@ -0,0 +1,17 @@
+package top.woaibocai.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: woaibocai-parent
+ * @description: 这里是关于一切用户所交互的接口 | 比如 评论接口 、 登录 、 注册 .....
+ * @author: woaibocai
+ * @create: 2023-12-09 14:59
+ **/
+@SpringBootApplication
+public class ServiceUserApplication16281 {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceUserApplication16281.class,args);
+    }
+}
