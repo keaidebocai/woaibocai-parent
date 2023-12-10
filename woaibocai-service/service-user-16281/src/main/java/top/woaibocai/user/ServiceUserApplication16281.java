e1c24d912e0e022f4d3fcc5f44fc923b03678872
woaibocai <365191806@qq.com>
2023-12-10 09:43:19 +0800

    添加扫描util模块的包

--- woaibocai-service/service-user-16281/src/main/java/top/woaibocai/user/ServiceUserApplication16281.java
+++ woaibocai-service/service-user-16281/src/main/java/top/woaibocai/user/ServiceUserApplication16281.java
@@ -2,6 +2,7 @@ package top.woaibocai.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @program: woaibocai-parent
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @create: 2023-12-09 14:59
  **/
 @SpringBootApplication(scanBasePackages = {"top.woaibocai.user","top.woaibocai.common"})
+@ComponentScan(basePackages = {"top.woaibocai"})
 public class ServiceUserApplication16281 {
     public static void main(String[] args) {
         SpringApplication.run(ServiceUserApplication16281.class,args);
