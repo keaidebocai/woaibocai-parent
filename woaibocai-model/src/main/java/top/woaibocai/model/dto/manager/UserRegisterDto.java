26b8cfbac1050f9b0e5becc889eeef40d51255a6
woaibocai <365191806@qq.com>
2023-12-14 19:11:45 +0800

    除去冗余

--- woaibocai-model/src/main/java/top/woaibocai/model/dto/manager/UserRegisterDto.java
+++ woaibocai-model/src/main/java/top/woaibocai/model/dto/manager/UserRegisterDto.java
@@ -1,9 +1,6 @@
 package top.woaibocai.model.dto.manager;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 /**
@@ -15,13 +12,10 @@ import lombok.Data;
 @Data
 @Schema(description = "注册数据接收")
 public class UserRegisterDto {
-    @NotNull
     @Schema(description = "用户名")
     private String userName;
-    @Max(value = 18,message = "最大长度不能超过18位")
     @Schema(description = "密码")
     private String password;
-    @Email(message = "邮箱格式不正确")
     @Schema(description = "邮箱")
     private String email;
 }
