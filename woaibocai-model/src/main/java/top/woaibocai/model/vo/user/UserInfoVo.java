b6add0597e35d42c1bef7fdfff925f5a4ee4f94f
woaibocai <365191806@qq.com>
2023-12-14 12:32:19 +0800

    userInfoVo

---
+++ woaibocai-model/src/main/java/top/woaibocai/model/vo/user/UserInfoVo.java
@@ -0,0 +1,21 @@
+package top.woaibocai.model.vo.user;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+/**
+ * @program: woaibocai-parent
+ * @description: 博客前端用户信息
+ * @author: woaibocai
+ * @create: 2023-12-14 09:20
+ **/
+@Data
+@Schema(description = "博客前端用户信息")
+public class UserInfoVo {
+    @Schema(description = "昵称")
+    private String nickName;
+    @Schema(description = "头像")
+    private String avatar;
+    @Schema(description = "性别 0:男，1:女，2:沃尔玛购物袋，3:阿帕奇武装直升机")
+    private String sex;
+}
