39ecc34053980ce349835f3b56ac4b78e00b462a
woaibocai <365191806@qq.com>
2023-12-15 11:55:56 +0800

    扫描common包

--- woaibocai-service/service-blog-16287/src/main/java/top/woaibocai/blog/ServiceBlogApplication16287.java
+++ woaibocai-service/service-blog-16287/src/main/java/top/woaibocai/blog/ServiceBlogApplication16287.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"top.woaibocai.blog","top.woaibocai.common"})
 public class ServiceBlogApplication16287 {
     public static void main(String[] args) {
         SpringApplication.run(ServiceBlogApplication16287.class,args);
