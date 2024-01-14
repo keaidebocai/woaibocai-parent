fba19389233e7e32b52c30ec83b0375bb3e3d58d
woaibocai <365191806@qq.com>
2024-01-14 11:26:42 +0800

    保存一级、二级评论、查询该用户的头像和名称、查询该文章的所有一级评论、根据id查询以及评论

--- woaibocai-service/service-blog-16287/src/main/java/top/woaibocai/blog/mapper/MyCommentMapper.java
+++ woaibocai-service/service-blog-16287/src/main/java/top/woaibocai/blog/mapper/MyCommentMapper.java
@@ -1,7 +1,24 @@
 package top.woaibocai.blog.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import top.woaibocai.model.Do.KeyAndValue;
+import top.woaibocai.model.dto.blog.comment.OneCommentDto;
+import top.woaibocai.model.dto.blog.comment.ReplyOneCommentDto;
+import top.woaibocai.model.vo.blog.comment.OneCommentVo;
+
+import java.util.List;
 
 @Mapper
 public interface MyCommentMapper {
+
+    KeyAndValue selectUserNameAndAvaterByUserId(String userId);
+
+    void saveComment(@Param("oneCommentDto") OneCommentDto oneCommentDto,@Param("id") String id);
+
+    List<String> selectOneCommentListByArticleId(String articleId);
+
+    OneCommentVo selectOneCommentById(String replyCommentId);
+
+    void saveSubComment(@Param("replyOneCommentVo") ReplyOneCommentDto replyOneCommentVo, @Param("id") String id);
 }
